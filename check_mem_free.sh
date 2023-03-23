af1bfd6890c62d9813538ed8ad44be785b806775
name <username.qwerty222@gmail.com>
2023-03-23 09:50:13 +0100

    Replace mem_free with mem_common before and in if/else

--- check_mem_free.sh
+++ check_mem_free.sh
@@ -8,19 +8,19 @@ mem_active=$(vmstat -aS M | sed -n '3 p' | sed 's/ [^0-9]*/|/g' | cut -d"|" -f7)
 mem_common=$(($mem_free + $mem_inact + $mem_active)) 
 mem_used=$(($mem_inact + $mem_active)) 
 
-# 25% / 50% / 75% of free memory equal to:
-mem_free_25=$(($mem_free / 4))
-mem_free_50=$(($mem_free / 2))
-mem_free_75=$(($mem_free_25 + $mem_free_50))
+# 25% / 50% / 75% of memory equal to:
+mem_common_25=$(($mem_common / 4))
+mem_common_50=$(($mem_common / 2))
+mem_common_75=$(($mem_common_25 + $mem_common_50))
 
 # compare used memory to free memory
-if (($mem_used < $mem_free_50)); then
+if (($mem_used < $mem_common_50)); then
         echo "Ok - memory usage less than 50%"
         exit 0
-    elif (($mem_free_50 <= $mem_used && $mem_used <= $mem_free_75)); then
+    elif (($mem_common_50 <= $mem_used && $mem_used <= $mem_common_75)); then
         echo "WARNING - memory usage is more than 50% but less then 75%"
         exit 1
-    elif (($mem_used >= $mem_free_75)); then
+    elif (($mem_used >= $mem_common_75)); then
         echo "CRITICAL - memory usage is more than 75%"
         exit 2
     else
